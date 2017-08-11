ef966391aabcef5694e7803755df1681bf95dc6e
cansusahin <30913876+cansusahin@users.noreply.github.com>
2017-08-11 12:35:38 +0300

    Add files via upload

---
+++ cansuozgesahin/main.py
@@ -0,0 +1,12 @@
+import webapp2
+import os #added
+from google.appengine.ext.webapp import template #also added
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), '/index.html')
+        self.response.out.write(template.render(path, {}))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
